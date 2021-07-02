5b51c5e5a2e92c5bbd9183fe0b775fe57b1bea20
brahim BAILEK <brahim.bailek@outlook.fr>
2021-07-02 16:57:09 +0200

    new

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -5,12 +5,20 @@ import Card from "./../components/Card";
 const Countries = () => {
   // initializer dta à un tab vide
   const [data, setData] = useState([]);
+  const [playOnce, setPlayOnce] = useState(true);
   useEffect(() => {
     axios
       .get(
         "https://www.restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
       )
       .then((res) => setData(res.data));
+      const sortedCountry = () => {
+        const countryObject = Object.keys(data).map((i) => data[i]);
+        const sortedArray = countryObject.sort((a, b) => {
+          return b.population - a.population;
+        });
+      };
+      sortedCountry();
   }, [data]);
 
   return (
